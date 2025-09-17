73f238d458e91fbfc0637b0b7e8cbb035b2d876e
Huzaifa Rehman <huzipro03@gmail.com>
2025-09-17 06:33:55 +0500

    dependencioes

--- main.py
+++ main.py
@@ -7,26 +7,63 @@ import pandas
 
 
 class Bayesian_network:
+    # given , given_value , tofind , value
     def __init__(self,answer_dict):
         self.data = pandas.read_csv("survey lung cancer.csv")
         attributes = self.data.columns
         for i in attributes:
-            setattr(self,i,answer_dict[i])
+            if i != "LUNG_CANCER":
+                setattr(self,i,answer_dict[i])
         
         P_dict = {}
         for i in attributes:
+            if i == "LUNG_CANCER":
+                continue
+            
             ans = getattr(self,i)
-            if not(isinstance(int,ans) or isinstance(str,ans)):
+            if (isinstance(int,ans) or isinstance(str,ans)):
                 # not needed in if gender or age
+                continue
+            # calculate required probabilities direct to lungcancer
+            prob = self.find_P(i,ans,"LUNG_CANCER","YES")
+            P_dict[i] = prob
+        
+        print(P_dict)
+            
+    def DependenciesOther(self):
+        # given , given_value , tofind , tofind_value but here tofing is not cancer
+        depend = [()]
+        True_prob = []
+        False_prob = []
+        for tup  in depend:
+            pa1,pa2,pa3,pa4 = tup
+            prob = self.find_P(pa1,pa2,pa3,pa4)
+            True_prob.append(prob) #all non negated Prob
+            False_prob.append( round(1-prob,6) ) # all negated Prob
+        return True_prob,False_prob    
                 
-                pass
+                 
                     
                     
             
                    
         #self.P_smoking = None # if he smoke or not than P of lungcancer
         
-
+    def find_P(self, given, given_value, tofind, tofind_value):
+        """
+        Find the probability P(tofind=tofind_value | given=given_value)
+        using frequency counts from self.data.
+        """
+        # Filter rows where given == given_value
+        filtered = self.data[self.data[given] == given_value]
+        # Count where tofind == tofind_value in filtered
+        match_count = (filtered[tofind] == tofind_value).sum()
+        total_count = len(filtered)
+        if total_count == 0:
+            return 0.0
+        probability = round(match_count / total_count,6)
+        return probability
+        
 
 
 class HealthSurveyGUI:
@@ -120,4 +157,4 @@ class HealthSurveyGUI:
 if __name__ == "__main__":
     gui = HealthSurveyGUI()
     ans = gui.start()
-    print(ans)
+    BN = Bayesian_network(ans)
